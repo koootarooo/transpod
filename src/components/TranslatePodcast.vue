a0c6a68099ee83a7c0dde2427713ad84a2f0ec95
koootarooo <iinemasuzawa@gmail.com>
2023-08-12 15:25:14 +0900

    add laoding animation

--- src/components/TranslatePodcast.vue
+++ src/components/TranslatePodcast.vue
@@ -11,7 +11,11 @@
       <p>{{file_title}}</p>
       <p>{{file_size}}</p>
     </div>
-    <button class="translate_btn" @click="translate()">translate</button>
+    <button class="translate_btn" @click="translate()" v-if="!isLoading">translate</button>
+    <div class="fulfilling-bouncing-circle-spinner" v-if="isLoading">
+      <div class="circle"></div>
+      <div class="orbit"></div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +30,7 @@ export default {
       file_title: "",
       file_size: "",
       isEnter: false,
+      isLoading: false
     }
   },
   methods: {
@@ -44,6 +49,7 @@ export default {
     },
     translate() {
       console.log('translate')
+      this.isLoading = true
     }
   }
 }
@@ -103,4 +109,111 @@ export default {
 	color: #27acd9;
 	background: #fff;
 }
+/* 以下アニメーション */
+.fulfilling-bouncing-circle-spinner, .fulfilling-bouncing-circle-spinner * {
+  box-sizing: border-box;
+}
+
+.fulfilling-bouncing-circle-spinner {
+  height: 60px;
+  width: 60px;
+	display: flex;
+  align-self: center;
+  animation: fulfilling-bouncing-circle-spinner-animation infinite 4000ms ease;
+}
+
+.fulfilling-bouncing-circle-spinner .orbit {
+  height: 60px;
+  width: 60px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 50%;
+  border: calc(60px * 0.03) solid #27acd9;
+  animation: fulfilling-bouncing-circle-spinner-orbit-animation infinite 4000ms ease;
+}
+
+.fulfilling-bouncing-circle-spinner .circle {
+  height: 60px;
+  width: 60px;
+  color: #27acd9;
+  display: block;
+  border-radius: 50%;
+  position: relative;
+  border: calc(60px * 0.1) solid #27acd9;
+  animation: fulfilling-bouncing-circle-spinner-circle-animation infinite 4000ms ease;
+  transform: rotate(0deg) scale(1);
+}
+
+@keyframes fulfilling-bouncing-circle-spinner-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+  }
+
+@keyframes fulfilling-bouncing-circle-spinner-orbit-animation {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1);
+  }
+  62.5% {
+    transform: scale(0.8);
+  }
+  75% {
+    transform: scale(1);
+  }
+  87.5% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes fulfilling-bouncing-circle-spinner-circle-animation {
+  0% {
+    transform: scale(1);
+    border-color: transparent;
+    border-top-color: inherit;
+  }
+  16.7% {
+    border-color: transparent;
+    border-top-color: initial;
+    border-right-color: initial;
+  }
+  33.4% {
+    border-color: transparent;
+    border-top-color: inherit;
+    border-right-color: inherit;
+    border-bottom-color: inherit;
+  }
+  50% {
+    border-color: inherit;
+    transform: scale(1);
+  }
+  62.5% {
+    border-color: inherit;
+    transform: scale(1.4);
+  }
+  75% {
+    border-color: inherit;
+    transform: scale(1);
+    opacity: 1;
+  }
+  87.5% {
+    border-color: inherit;
+    transform: scale(1.4);
+  }
+  100% {
+    border-color: transparent;
+    border-top-color: inherit;
+    transform: scale(1);
+  }
+} 
 </style>
